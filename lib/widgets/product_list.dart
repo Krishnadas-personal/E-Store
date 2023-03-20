1e26761516d01b180427d1f533d1a006a93ab2f5
Krishnadas <v.k.krishnadas3@gmail.com>
2023-03-20 18:07:03 +0530

    Added the discount Percentage

--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -14,96 +14,104 @@ class ProductsList extends StatelessWidget {
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 2,
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15)),
-                child: CarouselSlider(
-                    options: CarouselOptions(
-                      aspectRatio: 2.0,
-                      enlargeCenterPage: true,
-                      enlargeStrategy: CenterPageEnlargeStrategy.height,
-                      enlargeFactor: 0.4,
-                    ),
-                    items: product.images
-                        .map((img) => Image.network(img))
-                        .toList()),
-              ),
-              Positioned(
-                bottom: 10,
-                right: 5,
-                child: Container(
-                  padding: const EdgeInsets.all(4.0),
-                  color: Colors.black54,
-                  width: 300,
-                  child: Text(
-                    product.title,
-                    textAlign: TextAlign.right,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
               children: [
-                Row(
-                  children: [
-                    const Icon(Icons.branding_watermark),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      product.brand,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ],
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: CarouselSlider(
+                      options: CarouselOptions(
+                        aspectRatio: 2.0,
+                        enlargeCenterPage: true,
+                        enlargeStrategy: CenterPageEnlargeStrategy.height,
+                        enlargeFactor: 0.4,
+                      ),
+                      items: product.images
+                          .map((img) => Image.network(img))
+                          .toList()),
                 ),
-                Row(
-                  children: [
-                    const Icon(Icons.rate_review),
-                    const SizedBox(
-                      width: 5,
+                Positioned(
+                  bottom: 10,
+                  right: 5,
+                  child: Container(
+                    padding: const EdgeInsets.all(4.0),
+                    color: Colors.black54,
+                    width: 300,
+                    child: Text(
+                      product.title,
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
                     ),
-                    Text(
-                      product.rating.toString(),
-                      style: Theme.of(context).textTheme.titleMedium,
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    const Icon(Icons.attach_money_outlined),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      product.price.toString(),
-                      style: Theme.of(context).textTheme.titleMedium,
-                    )
-                  ],
-                ),
+                  ),
+                )
               ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.branding_watermark),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        product.brand,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.rate_review),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        product.rating.toString(),
+                        style: Theme.of(context).textTheme.titleMedium,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money_outlined),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        product.price.toString(),
+                        style: Theme.of(context).textTheme.titleMedium,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text(
               product.description,
               style: TextStyle(color: Colors.grey[700], fontSize: 15),
             ),
-          )
-        ],
+            Text(
+              'Discount%- ${product.discountPercentage}',
+              style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
